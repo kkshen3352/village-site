77a87ebfdb1dc8e9e2a1f6ef252854a90cce933f
villager <villager@pop-os.localdomain>
2020-04-24 00:55:34 +0800

    Add mdx support

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,53 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/pages/posts`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: `${__dirname}/src/pages/posts`,
+      },
+    },
+    // `gatsby-plugin-mdx`,
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        defaultLayouts: {
+          posts: require.resolve("./src/components/md/posts-layout.js"),
+          default: require.resolve("./src/components/md/default-page-layout.js"),
+        },
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 590,
+              shouldBlockNodeFromTransformation(node) {
+                return (
+                  [`NPMPackage`, `NPMPackageReadme`].includes(
+                    node.internal.type
+                  ) ||
+                  (node.internal.type === `File` &&
+                    path.parse(node.dir).dir.endsWith(`packages`))
+                )
+              },
+            },
+          },
+        ],
+      },
+    },
   ],
 }
