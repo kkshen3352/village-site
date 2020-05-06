8c882b0ff4fe98af1216d417fd19785c4a6c222e
villager <villager@pop-os.localdomain>
2020-05-07 00:24:43 +0800

    Add custom heading

--- src/components/md/provider.js
+++ src/components/md/provider.js
@@ -1,21 +1,36 @@
-import React from "react"
+import React, { useState } from "react"
 import { MDXProvider } from "@mdx-js/react"
 import { Link } from "gatsby"
-import { Heading } from '@chakra-ui/core'
+import { Box } from "@chakra-ui/core"
+import { jsx, css } from "@emotion/core"
+import CustomHeading from "./custom-heading"
 
 const shortcodes = { Link }
 
-const MyH1 = props => <h1 style={{ color: "tomato" }} {...props} />
-const MyParagraph = props => <p style={{ fontSize: "18px", lineHeight: 1.6 }} />
+const h1 = props => <CustomHeading as="h1" size="2xl" {...props} />
+const h2 = props => <CustomHeading as="h2" size="xl" {...props} />
+const h3 = props => <CustomHeading as="h3" size="lg" {...props} />
+const h4 = props => <CustomHeading as="h4" size="md" {...props} />
+const h5 = props => <CustomHeading as="h5" size="sm" {...props} />
+const h6 = props => <CustomHeading as="h6" size="xs" {...props} />
+const p = props => (
+  <p style={{ fontSize: "18px", lineHeight: 1.6 }} {...props} />
+)
 
 const components = {
-  h1: MyH1,
-  p: MyParagraph,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  ol,
   ...shortcodes,
 }
 
 export const Provider = ({ children }) => (
-  <main>
+  <Box as="main" p={{ base: 8, lg: 16 }}>
     <MDXProvider components={components}>{children}</MDXProvider>
-  </main>
+  </Box>
 )
--- src/pages/posts/markdown-cheatsheet.md
+++ src/pages/posts/markdown-cheatsheet.md
@@ -4,10 +4,15 @@
 ## Header
 
 # H1
+
 ## H2
+
 ### H3
+
 #### H4
+
 ##### H5
+
 ###### H6
 
 Alternatively, for H1 and H2, an underline-ish style:
