01c5614df7f15eb417e75858cc6005cf61c2c110
ClarityAcuity <zonapha770709@hotmail.com>
2020-08-02 22:02:39 +0800

    Add post provider

--- src/components/md/posts-layout.js
+++ src/components/md/posts-layout.js
@@ -4,10 +4,9 @@ import Header from "../header"
 import Footer from "../footer"
 
 export default ({ children }) => (
-  <div>
+  <>
     <Header siteTitle="Village" />
-    <h1>Posts Layout</h1>
     <Provider>{children}</Provider>
     <Footer />
-  </div>
+  </>
 )
--- src/components/md/provider.js
+++ src/components/md/provider.js
@@ -1,31 +1,58 @@
 import React, { useState } from "react"
 import { MDXProvider } from "@mdx-js/react"
-import { Link } from "gatsby"
-import { Box } from "@chakra-ui/core"
+import { Link as GatsbyLink } from "gatsby"
+import { Box, List, ListItem, Link, Image, Code } from "@chakra-ui/core"
 import { jsx, css } from "@emotion/core"
 import CustomHeading from "./custom-heading"
 
 const shortcodes = { Link }
 
-const h1 = props => <CustomHeading as="h1" size="2xl" {...props} />
-const h2 = props => <CustomHeading as="h2" size="xl" {...props} />
-const h3 = props => <CustomHeading as="h3" size="lg" {...props} />
-const h4 = props => <CustomHeading as="h4" size="md" {...props} />
-const h5 = props => <CustomHeading as="h5" size="sm" {...props} />
-const h6 = props => <CustomHeading as="h6" size="xs" {...props} />
-const p = props => (
-  <p style={{ fontSize: "18px", lineHeight: 1.6 }} {...props} />
-)
-
 const components = {
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6,
-  p,
-  ol,
+  h1: props => <CustomHeading as="h1" size="2xl" {...props} />,
+  h2: props => <CustomHeading as="h2" size="xl" {...props} />,
+  h3: props => <CustomHeading as="h3" size="lg" {...props} />,
+  h4: props => <CustomHeading as="h4" size="md" {...props} />,
+  h5: props => <CustomHeading as="h5" size="sm" {...props} />,
+  h6: props => <CustomHeading as="h6" size="xs" {...props} />,
+
+  p: props => <p style={{ fontSize: "18px", lineHeight: 1.6 }} {...props} />,
+
+  ol: props => <List as="ol" styleType="decimal" {...props} />,
+  ul: props => <List as="ul" {...props} styleType="disc" />,
+  li: props => <ListItem {...props} />,
+
+  a: props => <Link color="#0366d6" {...props} />,
+
+  img: props => <Image {...props} />,
+
+  // ``` ``` => <pre><code></code></pre>, ` ` => <code></code>
+  pre: props => (
+    <Box as="pre" mb="1.45rem" backgroundColor="gray.100" {...props} />
+  ),
+  code: props => <Code variantColor="gray.100" {...props} />,
+
+  table: props => <Box as="table" {...props} />,
+  thead: props => <Box as="thead" {...props} />,
+  tr: props => <Box as="tr" {...props} />,
+  th: props => <Box as="th" {...props} />,
+  tbody: props => <Box as="tbody" {...props} />,
+  td: props => <Box as="td" {...props} />,
+
+  blockquote: props => (
+    <Box
+      as="blockquote"
+      p={[0, 4]}
+      color="gray.200"
+      borderLeft=".25em solid gray.100"
+      // css={css`
+      //   padding: 0 1em;
+      //   color: #6a737d;
+      //   border-left: 0.25em solid #dfe2e5;
+      // `}
+      {...props}
+    />
+  ),
+
   ...shortcodes,
 }
 
