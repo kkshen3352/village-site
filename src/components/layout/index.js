b4f3ea0ed9a8494a8a9290dea7db0af3e287d107
ClarityAcuity <zonapha770709@hotmail.com>
2021-02-11 00:07:05 +0800

    Remove stylus

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -8,10 +8,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "../header"
 import Footer from "../footer"
-import "./style.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
