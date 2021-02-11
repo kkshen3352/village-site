03da049e557b3267505590a3f2807ddea4125a6a
ClarityAcuity <zonapha770709@hotmail.com>
2021-02-11 21:35:57 +0800

    Add posts list

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,72 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { Link, useStaticQuery, graphql } from "gatsby"
+import { Box, Heading, Flex, Text, Button } from "@chakra-ui/react"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-    <Link to="/geo">Go to geo page</Link>
-    <Link to="/rect">Go to rect page</Link>
-    <Link to="/rect-gl">Go to rect GL page</Link>
-    <Link to="/ray-tracing">Go to ray tracing page</Link>
-  </Layout>
-)
+function IndexPage({ data }) {
+  const query = useStaticQuery(graphql`
+    query IndexPageQuery {
+      allMdx(sort: { order: DESC, fields: frontmatter___date }) {
+        nodes {
+          id
+          slug
+          timeToRead
+          excerpt
+          frontmatter {
+            title
+            date
+            author
+          }
+        }
+      }
+    }
+  `)
+
+  console.log(query)
+  const {
+    allMdx: { nodes },
+  } = query
+
+  function _renderPosts() {
+    return nodes.map(
+      ({
+        id,
+        slug,
+        timeToRead,
+        excerpt,
+        frontmatter: { title, date, author },
+      }) => {
+        return (
+          <Box key={id}>
+            <h1>{title}</h1>
+            <div>
+              <span>{excerpt}</span>
+            </div>
+            <Flex>
+              <div>{timeToRead} min read</div>
+              <Link to={`/posts/${slug}`}>Read More</Link>
+            </Flex>
+            <Flex>
+              <div>{author}</div>
+              <div>{date}</div>
+            </Flex>
+          </Box>
+        )
+      }
+    )
+  }
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {_renderPosts()}
+      <Link to="/page-2/">Go to page 2</Link>
+      <Link to="/geo">Go to geo page</Link>
+      <Link to="/rect">Go to rect page</Link>
+      <Link to="/rect-gl">Go to rect GL page</Link>
+      <Link to="/ray-tracing">Go to ray tracing page</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
