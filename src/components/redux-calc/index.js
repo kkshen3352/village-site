ae8392960b812a5726b1160d9223bc8649d44f12
kkshen <h73423352@gmail.com>
2021-03-30 22:46:11 +0800

    add Middleware

--- src/components/redux-calc/index.js
+++ src/components/redux-calc/index.js
@@ -1,10 +1,25 @@
 import React from "react"
 import { connect, Provider } from "react-redux"
 import Calculator from "./components/Calculator"
-import store from "./store/store"
+// import store from "./store/store"
 import { mapDispatchToProps, mapStateToProps } from "./actions/actions"
+import { createStore, applyMiddleware } from 'redux';
+import reducer from "./reducers/rootReducer"
 
-const Calc = connect(mapStateToProps, mapDispatchToProps)(Calculator)
+const Calc = connect(mapStateToProps, mapDispatchToProps,)(Calculator)
+const logMiddleWare = store => next => action => {
+  console.log("Log Middleware:", action);
+  next(action);
+}
+// const logIncrementMiddleware = store => next => action => {
+//   if(action.type === 'EQUAL') {
+//     console.log(`EQUAL triggered: ${JSON.stringify(store.getState())}`);
+//   }
+//   next(action);
+// }
+const store = createStore(reducer, applyMiddleware(logMiddleWare, 
+  // logIncrementMiddleware
+  ));
 
 export default function() {
   return (
