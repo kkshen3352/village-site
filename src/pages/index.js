01741d0d5762744704edec7aec02b1399ca4fae0
kkshen <h73423352@gmail.com>
2021-04-11 03:06:50 +0800

    add image urlGoogle2

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,13 @@ import { Gatsbyicon } from "../icon/gatsbyicon"
 import { Chakraicon } from "../icon/chakrsicon"
 import { Graphqlicon } from "../icon/graphqlicon"
 import { Emotionicon } from "../icon/emotionicon"
+import urlGoogle2 from "../images/DSC_7875.jpg"
 
 function IndexPage({ data }) {
   const {
     site: { pathPrefix },
   } = data
-  const urlGoogle2 =
-    "https://lh3.googleusercontent.com/RJW5As6mPiqbG5Z2IwzDyw4Fy3f3Fw7D8LUz4l-Z4b6THVbZaN--EL08CRADl2Nqj8oxcP_nOABWa22auZB2B-0xbpo5ETdnyoQ1bccwKtx_vLJzGtcpddOUtAZ0h_zNMzNuvSUP1AYLvhHpL8CNG3ehJliJ6posY5RVRIMNoEPl3oDnxrlrb8MYYU6xtstOTyly-ghQIu565V15zAM5goxN4wpMP-oEIXrpZeTBgOencIMenI-bVUcX8JUVAY1Fojv1JKucSVV1Tna0Ldj5DDLBZNh6vZzLXMNr5EZ25_hFmcwAozHjSrZNT7rN3OZsysiTs76fwnpctpXJcclFI30VNgzncwu7t450PzdV4fvTk7JypXwJGf-RJciuS-4iD5GbMhh5TrfBhavZrbJMkFuFlxNchxRbcaQrXfxOOoRWGcgACXLfkMgRh01erFKfpBDkAfud0lJh92Ai8hucuGUJ4I7qajFUFzGHOhokegP6E6MnMzMgR5qDBqUQH3d9TdPHdIpF5TuWVPWnPhGLk8ZFwot2RP2jsxNILp2g0fwFsEjSbpXIIrHbU-X2pZl-w33ePzjQwI9-WYUHFyIekKwnw-eWT1a21mjnANdJ_jzcurkoVYVQjHvR5L1xM_kkSwMukg9JKu5_PHmlkIFdxmMPSb2GVd9EzU8bqgyuwenDj_vvOMQqB2GzIu3T1-uCqCDZg4FL1JIfCZZzVLp_7J1CVg=w1322-h881-no?authuser=0"
-
+  
   return (
     <Layout m="5">
       <SEO title="Home" />
